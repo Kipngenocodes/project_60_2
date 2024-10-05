8055bffae3a0b624133fb0640a349b34b692a22f
Kipngenocodes <kipngenoalex.c@gmail.com>
2024-10-05 06:25:53 -0700

    final project

--- Tcp_client.py
+++ Tcp_client.py
@@ -1,6 +1,6 @@
 import socket 
 
-target_host = "www.goggle.com"
+target_host = "http://www.google.com/"
 target_port = 80
 
 # create a socket object 
@@ -8,9 +8,11 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # connect the client
 client.connect((target_host, target_port))
+print(f"connected  to {target_host}:{target_port}")
+
 
 # send some data
-client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
 # receive some data
 data = client.recv(4096)
 
