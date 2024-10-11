580a3757ed9413e0ba64f0dac9221a5beef25ed0
Kipngenocodes <kipngenoalex.c@gmail.com>
2024-10-10 23:02:37 -0700

    sql injection

--- reverse shell/extraction_and_submitting.py
+++ reverse shell/extraction_and_submitting.py
@@ -1,17 +1,23 @@
+from selenium import webdriver
 from bs4 import BeautifulSoup
-from requests_html import HTMLSession
 from pprint import pprint
 
-# Initialize an HTTP session
-session = HTMLSession()
+# Initialize the Selenium WebDriver (using Chrome in this case)
+driver = webdriver.Chrome()  # Or replace with webdriver.Firefox() for Firefox
 
 def get_all_forms(url):
     """Returns all form tags found on a web page's 'url'"""
-    # GET request
-    res = session.get(url)
-    # For JavaScript driven websites, uncomment the line below
-    # res.html.render()
-    soup = BeautifulSoup(res.html.html, "html.parser")
+    # Open the URL
+    driver.get(url)
+    
+    # Wait for the page to load completely (optional, for JavaScript-heavy pages)
+    driver.implicitly_wait(5)
+    
+    # Get the page source after the JavaScript has executed
+    page_source = driver.page_source
+    
+    # Parse with BeautifulSoup
+    soup = BeautifulSoup(page_source, "html.parser")
     return soup.find_all("form")
     
 def get_form_details(form):
@@ -26,51 +32,34 @@ def get_form_details(form):
     
     # Process input tags
     for input_tag in form.find_all("input"):
-        # Get type of input form control
         input_type = input_tag.attrs.get("type", "text")
-        # Get name attribute
         input_name = input_tag.attrs.get("name")
-        # Get the default value of that input tag
         input_value = input_tag.attrs.get("value", "")
-        # Add everything to that list
         inputs.append({"type": input_type, "name": input_name, "value": input_value})
     
     # Process select tags
     for select in form.find_all("select"):
-        # Get the name attribute
         select_name = select.attrs.get("name")
-        # Set the type as select
         select_type = "select"
         select_options = []
-        # The default select value
         select_default_value = ""
-        # Iterate over options and get the value of each
         for select_option in select.find_all("option"):
-            # Get the option value used to submit the form
             option_value = select_option.attrs.get("value")
             if option_value:
                 select_options.append(option_value)
                 if select_option.attrs.get("selected"):
-                    # If 'selected' attribute is set, set this option as default    
                     select_default_value = option_value
         if not select_default_value and select_options:
-            # If the default is not set, and there are options, take the first option as default
             select_default_value = select_options[0]
-        # Add the select to the inputs list
         inputs.append({"type": select_type, "name": select_name, "values": select_options, "value": select_default_value})
 
     # Process textarea tags
     for textarea in form.find_all("textarea"):
-        # Get the name attribute
         textarea_name = textarea.attrs.get("name")
-        # Set the type as textarea
         textarea_type = "textarea"
-        # Get the textarea value
         textarea_value = textarea.text
-        # Add the textarea to the inputs list
         inputs.append({"type": textarea_type, "name": textarea_name, "value": textarea_value})
         
-    # Put everything to the resulting dictionary
     details["action"] = action
     details["method"] = method
     details["inputs"] = inputs
@@ -78,13 +67,17 @@ def get_form_details(form):
     return details
 
 if __name__ == "__main__":
-    import sys
-    # Get URL from the command line
-    url = sys.argv[1]
-    # Get all form tags
+    # Define the URL to be scraped
+    url = "https://www.amazon.com/"
     forms = get_all_forms(url)
-    # Iterate over forms
-    for i, form in enumerate(forms, start=1):
-        form_details = get_form_details(form)
-        print("="*50, f"form #{i}", "="*50)
-        pprint(form_details)
+    
+    if not forms:
+        print("No forms found on the page.")
+    else:
+        for i, form in enumerate(forms, start=1):
+            form_details = get_form_details(form)
+            print("="*50, f"form #{i}", "="*50)
+            pprint(form_details)
+    
+    # Close the browser window
+    driver.quit()
